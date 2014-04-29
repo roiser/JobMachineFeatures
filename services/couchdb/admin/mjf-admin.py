151b4e5a5bdb987c4cff80537c82dcddb5bfe883
Stefan Roiser <stefan.roiser@cern.ch>
2014-04-29 18:05:39 +0200

    initial commit of admin tool for key/value store based on couchdb

--- services/couchdb/admin/mjf-admin.py
+++ services/couchdb/admin/mjf-admin.py
@@ -2,8 +2,20 @@
 
 VERSION='0.0.1'
 
-from optparse import OptionParser
-import sys, json, urllib, httplib
+from optparse import OptionParser, IndentedHelpFormatter
+import sys, json, urllib, httplib, os
+
+# class PlainHelpFormatter(IndentedHelpFormatter):
+#     def format_description(self, description):
+#         if description:
+#             return description + "\n"
+#         else:
+#             return ""
+#     def format_epilog(self, epilog):
+#         if epilog:
+#             return epilog + "\n"
+#         else:
+#             return ""
 
 class log :
   def __init__(self, dbg=False, inf=True): 
@@ -28,7 +40,7 @@ class log :
 
 class mjfAdmin :
 
-  def __init__(self, server, port, database, bulk, selkey, value, logi=None):
+  def __init__(self, server, port, database, bulk, args, selkey, value, logi=None):
     self.log = log()
     if logi: self.log = logi
     self.server = server
@@ -37,13 +49,28 @@ class mjfAdmin :
     self.bulk = bulk
     self.selkey = selkey
     self.value = value
+    self.arguments = args
     self.fullserver = 'http://%s:%s' % (self.server, self.port)
-    self.headers = {"Content-type": "application/json"}
+    self.headers = {"Content-type": "application/json", "Accept-Encoding": "*"}
 
-  def _httprequest(self, typ, url, doc='', jsn=True) : 
+  def _strToJson(self, instr) :
+    # ugly hack, this needs to be fixed
+    newstr = str(instr).replace("u'", "'").replace("'", '"').replace("True","true").replace("False","false")
+    js = None
+    try : 
+      js = json.loads(newstr)
+    except Exception, e: 
+      self.log.error('conversion of string %s, %s' % (newstr, e))
+    return js
+
+  def _httprequest(self, typ, url, doc='', headeradd={}, jsn=True) : 
+    header = dict(self.headers.items() + headeradd.items())
     conn = httplib.HTTPConnection(self.server, port=self.port)
-    conn.request(typ, '/'+url, doc, self.headers)
-    ret = conn.getresponse()
+    if self.log.dbg : conn.set_debuglevel(99)
+    try: 
+      conn.request(typ, '/'+url, doc, header)
+      ret = conn.getresponse()
+    except Exception, e: self.log.error(e)
     self.log.debug('Status %s, Response: %s' % (ret.status, ret.reason))
     retval = ret.read()
     conn.close()
@@ -81,16 +108,40 @@ class mjfAdmin :
     else :
       self.log.error('Cannot connect to server %s, return message: %s' % (self.fullserver, str(info)))
     dbs = self._httprequest('GET', '_all_dbs')
-    self.log.info('Databases installed: %s' % dbs)
+    if self.database in dbs : self.log.info('Database %s installed' % self.database)
+    else : self.log.error('Database %s not installed on server, installed DBs are %s' % ( self.database, dbs))
 
   def _put(self) : 
-    self.log.debug('Putting document(s) on database %s at server %s' % (self.database, self.fullserver))
+    key = self.arguments[1]
+    value = self.arguments[2]
+    self.log.debug('Putting document(s) %s on database %s at server %s' % (key, self.database, self.fullserver))
+    inf = self._httprequest('PUT', self.database + os.sep + key, doc=value)
+    jinfo = self._strToJson(inf)
+    if jinfo.has_key('error') : self.log.error('Could not create post document %s, error=%s, reason=%s' % (key, jinfo.get('error'), jinfo.get('reason')))
+    else: self.log.info('Put successful '+ str(jinfo))
 
   def _get(self) : 
+    key = self.arguments[1]
     self.log.debug('Getting document(s) from database %s at server %s' % (self.database, self.fullserver))
+    inf = self._httprequest('GET', self.database + os.sep + key)
+    jinfo = self._strToJson(inf)
+    if jinfo.has_key('error') : self.log.error('Cannot get document %s, error=%s, reason=%s' % (key, jinfo.get('error'), jinfo.get('reason')))
+    else: self.log.info('Get successful' + str(jinfo))
 
   def _delete(self) : 
-    self.log.debug('Deleting document(s) from database %s at server %s' % (self.database, self.fullserver))
+    key = self.arguments[1]
+    self.log.debug('Deleting document(s) %s from database %s at server %s' % (key, self.database, self.fullserver))
+    inf = self._httprequest('GET', self.database + os.sep + key)
+    jinf = self._strToJson(inf)
+    if jinf.has_key('_rev'):
+      rev = jinf['_rev'] 
+      self.log.debug('Deleting object with key %s and revision %s' % ( key, rev ))
+      inf2 = self._httprequest('DELETE', self.database + os.sep + key, headeradd={"If-Match":rev})
+      jinf2 = self._strToJson(inf2)
+      if jinf2.has_key('error') : self.log.error('Cannot delete document %s, error=%s, reason=%s' % (key, jinf2.get('error'), jinf2.get('reason')))
+      else : self.log.info('Delete successful '+ str(jinf2))
+    elif jinf.has_key('error') : 
+      self.log.error('Cannot find revision for document %s, error=%s, reason=%s' % (key, jinf['error'], jinf.get('reason')))
 
   def _version(self): 
     print VERSION
@@ -99,14 +150,23 @@ class mjfAdmin :
 
 if __name__ == '__main__' : 
   actions = ['installdb','removedb','checkdb','version','put','get','delete']
-  parser = OptionParser()
+  usage = 'usage: %prog [options] [put <key> <value>] | [ get | delete <key>] | [ installdb | removedb | checkdb | version ]'
+  desc = """Arguments:\n
+   put:\n
+   get:\n
+   delete:\n 
+   installdb:\n 
+   removedb:\n
+   checkdb:\n
+   version:
+"""
+  parser = OptionParser(usage=usage) #, description=desc)
   parser.add_option('-q', '--quiet', action='store_false', default=True, dest='quiet', help='only print warning and error messages')
   parser.add_option('-v', '--verbose', action='store_true', default=False, dest='verbose', help='increase verbosity of output')
-  parser.add_option('-s', '--server', action='store', default='wlcg-mjf', dest='server', help='host name of the key value store (default="wlcg-mjf")')
+  parser.add_option('-s', '--server', action='store', default='wlcg-mjf.cern.ch', dest='server', help='host name of the key value store (default="wlcg-mjf")')
   parser.add_option('-p', '--port', action='store', default='5984', dest='port', help='port to access the web interface of the store (default=5984)')
   parser.add_option('-d', '--database', action='store', default='machinejobfeatures', dest='database', help='database name storing the features (default="machinejobfeatures")')
   parser.add_option('-b', '--bulk', action='store_true', default='False', dest='bulk', help='execute a bulk operation on multiple documents')
-
   
   action = ''
   selkey = ''
@@ -138,7 +198,7 @@ if __name__ == '__main__' :
 
   logi = log(options.verbose,options.quiet)
 
-  mad = mjfAdmin(options.server, options.port, options.database, options.bulk, selkey=selkey, value=value, logi=logi)
+  mad = mjfAdmin(options.server, options.port, options.database, options.bulk, args, selkey=selkey, value=value, logi=logi)
   if '_'+action in dir(mad) : eval('mad._'+action+'()')
   
 
@@ -163,3 +223,8 @@ if __name__ == '__main__' :
 # bulk put <selection> <value>
 # bulk get <selection>
 # bulk delete <selection>
+
+
+#  ./mjf-admin.py  put 113.3.0.3 '{"machinefeatures": {"hs06": 9.0999999999999996, "jobslots": 9, "shutdowntime": 1398790803.78, "log_cores": 8, "shutdown_command": "shutdown -h now", "phys_cores": 15}, "jobfeatures": {"disk_limit_GB": 138, "jobstart_secs": 1398783794.78, "cpufactor_lrms": 9.9900000000000002, "cpu_limit_secs": 65798, "cpu_limit_secs_lrms": 85504, "allocated_CPU": 6, "mem_limit_MB": 4406, "wall_limit_secs_lrms": 80541, "wall_limit_secs": 76355, "shutdowntime_job": 1398793344.78}}'
+#
+# ./mjf-admin.py: INFO   : Put successful {u'ok': True, u'rev': u'1-b50910ed8b66afde135d7137dbee6266', u'id': u'113.3.0.3'}
