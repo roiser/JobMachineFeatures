4d105b6b302a4763b6ca41cf0756883bb2a02221
Stefan Roiser <stefan.roiser@cern.ch>
2014-12-09 21:22:08 +0100

    remove couchdb implementation

--- services/couchdb/couchapp/auth/validate_doc_update.js
+++
@@ -1,7 +0,0 @@
-function(newDoc, oldDoc, userCtx) { 
-  if (userCtx.roles.indexOf('_admin') !== -1) { 
-    return; 
-  } else { 
-   throw({forbidden: 'read-only database'});
-  } 
-}
--- services/couchdb/couchapp/do/shows/jobfeature.js
+++
@@ -1,7 +0,0 @@
-function(doc, req) { 
-  log(req); 
-  return { 'code': 200, 
-           'headers': { 'content-type': 'text/plain' }, 
-            body: doc['jobfeatures'][req.query.name].toString() 
-  } 
-}
