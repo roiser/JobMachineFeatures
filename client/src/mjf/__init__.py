67de1e68d741d80142423034118d59c341bc95ca
Stefan Roiser <stefan.roiser@cern.ch>
2014-12-09 21:19:26 +0100

    remove client

--- client/src/mjf/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.1'
-__all__ = ['mjf', 'MJFException']
-__author__ = 'Stefan Roiser <stefan.roiser@cern.ch>'
-
-from mjf import mjf, MJFException
--- client/test/setup.sh
+++
@@ -1,5 +0,0 @@
-d="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd $d > /dev/null 2>&1
-export MACHINEFEATURES=`pwd`/m
-export JOBFEATURES=`pwd`/j
-popd > /dev/null 2>&1
